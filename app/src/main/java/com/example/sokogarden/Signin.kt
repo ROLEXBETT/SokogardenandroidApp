3c05dc4d0d7e7157aed2cf63cddf77fe1008fcfc
ROLEXBETT <rolexbett6684@gmail.com>
2026-04-13 20:23:08 +0300

    updated

--- app/src/main/java/com/example/sokogarden/Signin.kt
+++ app/src/main/java/com/example/sokogarden/Signin.kt
@@ -1,10 +1,15 @@
 package com.example.sokogarden
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class Signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,30 @@ class Signin : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val signinBtn = findViewById<Button>(R.id.signinBtn)
+        val signupTextView = findViewById<TextView>(R.id.signuptxt)
+
+        signupTextView.setOnClickListener {
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+        }
+
+        signinBtn.setOnClickListener {
+            val api = "https://kbenkamotho.alwaysdata.net/api/signin"
+            val params = RequestParams()
+
+            params.put("email", email.text.toString())
+            params.put("password", password.text.toString())
+
+            // Initialize the API helper (Case-sensitive: ApiHelper)
+            val helper = ApiHelper(applicationContext)
+
+            // Use the post_login function from ApiHelper with the correct 'params' object
+            helper.post_login(api, params)
         }
     }
 }
